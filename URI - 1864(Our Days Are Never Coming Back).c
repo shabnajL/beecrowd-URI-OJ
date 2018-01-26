99400f2c8f48b570941f5415c59ac4a72d696714
Su_rose19 <shabnajliaqat@gmail.com>
2018-01-27 00:13:29 +0600

    Create URI - 1864(Our Days Are Never Coming Back).c

---
+++ URI - 1864(Our Days Are Never Coming Back).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+char arr[]={"LIFE IS NOT A PROBLEM TO BE SOLVED"};
+int main()
+{
+    int N,i;
+    scanf("%d",&N);
+    for(i=0;i<N;i++)
+    {
+        printf("%c",arr[i]);
+    }
+    printf("\n");
+}
