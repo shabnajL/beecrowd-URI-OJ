75c01dfbf47949cf4cb59da49d3b9f41f0ec2ca6
Su_rose19 <shabnajliaqat@gmail.com>
2021-04-02 10:02:17 +0600

    Add files via upload

---
+++ URI-2058.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+//using namespace std;
+int main()
+{
+    int N,res;
+    std::cin>>N;
+    res = N- 2;
+    std::cout<<res<<std::endl;
+    return 0;
+}
---
+++ URI-2413.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+//using namespace std;
+int main()
+{
+    int N,res;
+    std::cin>>N;
+    res = 4 * N;
+    std::cout<<res<<std::endl;
+    return 0;
+}
+
