d428b09ded9a07b88de2a0f294b72687aaaf062e
Shabnaj L. <shabnajliaqat@gmail.com>
2024-10-30 02:11:09 +0600

    Create Bee -  2748(Output 2).py

---
+++ Bee -  2748(Output 2).py
@@ -0,0 +1,13 @@
+s1 = "---------------------------------------"
+s2 = "|                                     |"
+for i in range(7):
+    if(i==0 or i==6):
+        print(s1)
+    elif(i%2==0):
+        print(s2)
+    elif(i==1):
+        print("|        Roberto                      |")
+    elif(i==3):
+        print("|        5786                         |")
+    elif(i==5):
+        print("|        UNIFEI                       |")
